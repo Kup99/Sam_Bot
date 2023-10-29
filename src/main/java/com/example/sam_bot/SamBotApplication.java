23a2ee7f993e7f16ee0c5d96ceb6e26b148a9839
kirillbatutin <alien24679@yandex.ru>
2023-10-29 14:23:23 +0400

    test

--- src/main/java/com/example/sam_bot/SamBotApplication.java
+++ src/main/java/com/example/sam_bot/SamBotApplication.java
@@ -1,14 +1,17 @@
 package com.example.sam_bot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
-//@ComponentScan(basePackages={"com.example.sam_bot.MessageService"})
+@ComponentScan(basePackages = "com.example.sam_bot.*")
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories("com.example.sam_bot.repository")
+@EntityScan("com.example.sam_bot.model")
+
 public class SamBotApplication {
 
     public SamBotApplication() {
--- src/main/java/com/example/sam_bot/service/MessageService.java
+++ src/main/java/com/example/sam_bot/service/MessageService.java
@@ -4,18 +4,18 @@ import com.example.sam_bot.model.Message;
 import com.example.sam_bot.repository.MessageRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @NoArgsConstructor
 @Service
 public class MessageService {
 
     @Autowired
-    private MessageRepository messageRepository;
+    MessageRepository messageRepository;
 
     public void createMessage(Message message) {
         messageRepository.save(message);
